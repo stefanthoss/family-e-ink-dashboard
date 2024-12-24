a657c2ff24dd2078d4727373d8714e09ff5a8d77
Stefan Thoss <stefanthoss@users.noreply.github.com>
2024-12-23 20:57:55 -0800

    Fix all day events not showing for today (#15)
    * Show all day events for today
    * poetry update
    * Bump version

--- src/ics_cal/icshelper.py
+++ src/ics_cal/icshelper.py
@@ -34,37 +34,38 @@ class IcsHelper:
         if not cal.events:
             self.logger.info("No upcoming calendar events found.")
         for event in cal.events:
-            if event.begin >= arrow.Arrow.fromdatetime(
-                startDatetime
-            ) and event.begin < arrow.Arrow.fromdatetime(endDatetime):
-                # extracting and converting events data into a new list
-                new_event = {"summary": event.name}
+            # extracting and converting events data into a new list
+            new_event = {"summary": event.name}
 
-                if event.location:
-                    new_event["location"] = event.location
+            if event.location:
+                new_event["location"] = event.location
 
-                new_event["allday"] = event.all_day
-                if new_event["allday"]:
-                    # All-day events are always midnight UTC to midnight UTC, therefore timezone needs to be set
-                    new_event["startDatetime"] = dt.datetime.fromisoformat(
-                        event.begin.replace(tzinfo=localTZ).isoformat()
-                    )
-                    new_event["endDatetime"] = dt.datetime.fromisoformat(
-                        event.end.replace(tzinfo=localTZ).isoformat()
-                    )
-                else:
-                    # Other events need to be translated to local timezone
-                    new_event["startDatetime"] = dt.datetime.fromisoformat(
-                        event.begin.to(localTZ).isoformat()
-                    )
-                    new_event["endDatetime"] = dt.datetime.fromisoformat(
-                        event.end.to(localTZ).isoformat()
-                    )
+            new_event["allday"] = event.all_day
+            if new_event["allday"]:
+                # All-day events are always midnight UTC to midnight UTC, therefore timezone needs to be set
+                new_event["startDatetime"] = dt.datetime.fromisoformat(
+                    event.begin.replace(tzinfo=localTZ).isoformat()
+                )
+                new_event["endDatetime"] = dt.datetime.fromisoformat(
+                    event.end.replace(tzinfo=localTZ).isoformat()
+                )
+            else:
+                # Other events need to be translated to local timezone
+                new_event["startDatetime"] = dt.datetime.fromisoformat(
+                    event.begin.to(localTZ).isoformat()
+                )
+                new_event["endDatetime"] = dt.datetime.fromisoformat(
+                    event.end.to(localTZ).isoformat()
+                )
 
-                new_event["isMultiday"] = (
-                    new_event["endDatetime"] - new_event["startDatetime"]
-                ) > dt.timedelta(days=1)
+            new_event["isMultiday"] = (
+                new_event["endDatetime"] - new_event["startDatetime"]
+            ) > dt.timedelta(days=1)
 
+            if (
+                new_event["startDatetime"] >= startDatetime
+                and new_event["startDatetime"] < endDatetime
+            ):
                 event_list.append(new_event)
 
         return sorted(event_list, key=lambda k: k["startDatetime"])
