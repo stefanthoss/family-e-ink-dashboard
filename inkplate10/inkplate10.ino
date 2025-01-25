bab1c90d61530221a17aa64f17fb3611e6dad5d3
Stefan Thoss <git.identity164@slmails.com>
2025-01-24 20:37:40 -0800

    Move helper function to separate file

--- inkplate10/inkplate10.ino
+++ inkplate10/inkplate10.ino
@@ -21,16 +21,7 @@
 #include "Inkplate.h"
 #include "WiFi.h"
 
-// Needed for drawPngFromBuffer function, workaround until https://github.com/SolderedElectronics/Inkplate-Arduino-library/pull/210 is merged
-#include "libs/pngle/pngle.h"
-uint8_t ditherBuffer[2][E_INK_WIDTH + 20];
-extern Image *_imagePtrPng;
-static bool _pngInvert = 0;
-static bool _pngDither = 0;
-static int16_t lastY = -1;
-static uint16_t _pngX = 0;
-static uint16_t _pngY = 0;
-static Image::Position _pngPosition = Image::_npos;
+#include "lib/png_image_helper.cpp"
 
 char *ssid = "YOUR WIFI SSID";  // Your WiFi SSID
 char *pass = "YOUR WIFI PASSWORD";  // Your WiFi password
@@ -123,9 +114,6 @@ void setup()
 
             // Draw image into the frame buffer of Inkplate
             drawPngFromBuffer(buffer, size, 0, 0, true, false);
-
-            // Free the memory where the image was stored because it is now in the frame buffer
-            free(buffer);
         }
         else
         {
@@ -144,16 +132,16 @@ void setup()
     // Show battery percentage if low
     double battvoltage = display.readBattery();
     int battpc = calc_battery_percentage(battvoltage);
-   // if (battvoltage < BATTV_LOW) {
-        display.setCursor(1050, 800);  // Inkplate 10 has a 9.7 inch, 1,200 x 825 pixel display
-        display.println("Battery: " + String(battpc) + " %");
-   // }
+    if (battvoltage < BATTV_LOW) {
+        display.setCursor(1025, 800);  // Inkplate 10 has a 9.7 inch, 1,200 x 825 pixel display
+        display.println("Battery: " + String(battpc) + "%");
+    }
 
     // Draw image on the screen
     display.display();
 
     // Go to sleep for 60min (60min * 60s * 1000ms * 1000us)
-    esp_sleep_enable_timer_wakeup(30ll * 60 * 1000 * 1000);
+    esp_sleep_enable_timer_wakeup(60ll * 60 * 1000 * 1000);
 
     // Enable wakeup from deep sleep on gpio 36 (wake button)
     esp_sleep_enable_ext0_wakeup(GPIO_NUM_36, LOW);
@@ -178,125 +166,4 @@ int calc_battery_percentage(double battv)
         battery_percentage = 100;
 
     return battery_percentage;
-}
-
-/**
- * @brief       drawPngFromBuffer function draws png image from buffer (copied from unmerged PR https://github.com/SolderedElectronics/Inkplate-Arduino-library/pull/210)
- *
- * @param       int32_t len
- *              size of buffer
- * @param       int x
- *              x position for top left image corner
- * @param       int y
- *              y position for top left image corner
- * @param       bool dither
- *              1 if using dither, 0 if not
- * @param       bool invert
- *              1 if using invert, 0 if not
- *
- * @return      1 if drawn successfully, 0 if not
- */
-bool drawPngFromBuffer(uint8_t *buf, int32_t len, int x, int y, bool dither, bool invert)
-{
-    if (!buf)
-        return 0;
-
-    _pngDither = dither;
-    _pngInvert = invert;
-    lastY = y;
-
-    bool ret = 1;
-
-    if (dither)
-        memset(ditherBuffer, 0, sizeof ditherBuffer);
-
-    pngle_t *pngle = pngle_new();
-    _pngX = x;
-    _pngY = y;
-    pngle_set_draw_callback(pngle, pngle_on_draw_mod);
-
-    if (!buf)
-        return 0;
-
-    if (pngle_feed(pngle, buf, len) < 0)
-        ret = 0;
-
-    pngle_destroy(pngle);
-    free(buf);
-    return ret;
-}
-
-/**
- * @brief       pngle_on_draw
- *
- * @param       pngle_t *pngle
- *              pointer to image
- * @param       uint32_t x
- *              x plane position
- * @param       uint32_t y
- *              y plane position
- * @param       uint32_t w
- *              image width
- * @param       uint32_t h
- *              image height
- * @param       uint8_t rgba[4]
- *              color
- */
-void pngle_on_draw_mod(pngle_t *pngle, uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint8_t rgba[4])
-{
-    if (_pngPosition != Image::_npos)
-    {
-        _imagePtrPng->getPointsForPosition(_pngPosition, pngle_get_width(pngle), pngle_get_height(pngle), E_INK_WIDTH,
-                                           E_INK_HEIGHT, &_pngX, &_pngY);
-        lastY = _pngY;
-        _pngPosition = Image::_npos;
-    }
-    if (rgba[3])
-        for (int j = 0; j < h; ++j)
-            for (int i = 0; i < w; ++i)
-            {
-                uint8_t r = rgba[0];
-                uint8_t g = rgba[1];
-                uint8_t b = rgba[2];
-
-                pngle_ihdr_t *ihdr = pngle_get_ihdr(pngle);
-
-                if (ihdr->depth == 1)
-                    r = g = b = (b ? 0xFF : 0);
-
-#if defined(ARDUINO_INKPLATECOLOR) || defined(ARDUINO_INKPLATE2) || defined(ARDUINO_INKPLATE4) ||                      \
-    defined(ARDUINO_INKPLATE7)
-                if (_pngInvert)
-                {
-                    r = 255 - r;
-                    g = 255 - g;
-                    b = 255 - b;
-                }
-
-                uint8_t px = _imagePtrPng->findClosestPalette((r << 16) | (g << 8) | (b));
-#else
-                uint8_t px = RGB3BIT(r, g, b);
-#endif
-
-                if (_pngDither)
-                {
-#if defined(ARDUINO_INKPLATECOLOR) || defined(ARDUINO_INKPLATE2) || defined(ARDUINO_INKPLATE4) ||                      \
-    defined(ARDUINO_INKPLATE7)
-                    px = _imagePtrPng->ditherGetPixelBmp((r << 16) | (g << 8) | (b), x + i, y + j,
-                                                         _imagePtrPng->width(), 0);
-#else
-                    px = _imagePtrPng->ditherGetPixelBmp(RGB8BIT(r, g, b), x + i, y + j, _imagePtrPng->width(), 0);
-                    if (_pngInvert)
-                        px = 7 - px;
-                    if (_imagePtrPng->getDisplayMode() == INKPLATE_1BIT)
-                        px = (~px >> 2) & 1;
-#endif
-                }
-                _imagePtrPng->drawPixel(_pngX + x + i, _pngY + y + j, px);
-            }
-    if (lastY != y)
-    {
-        lastY = y;
-        _imagePtrPng->ditherSwap(_imagePtrPng->width());
-    }
 }
