5c35194e82ddf6878dc97b5d4888c369f9c49e7b
Stefano Guandalini <github@guandalf.com>
2025-08-06 17:47:39 +0200

    Support for multiple calendars

--- src/ics_cal/icshelper.py
+++ src/ics_cal/icshelper.py
@@ -23,52 +23,57 @@ class IcsHelper:
         event_list = []
 
         self.logger.info("Retrieving events from ICS...")
-        response = requests.get(ics_url)
-        if response.ok:
-            cal = icalendar.Calendar.from_ical(response.text)
-        else:
-            self.logger.error(f"Received an error when downloading ICS: {response.text}")
-            sys.exit(1)
-
-        events = recurring_ical_events.of(cal).between(calStartDatetime, calEndDatetime)
-        local_timezone = pytz.timezone(localTZ)
-
-        for event in events:
-            new_event = {"summary": str(event.get("SUMMARY"))}
-
-            if "LOCATION" in event:
-                new_event["location"] = str(event.get("LOCATION"))
-
-            event_start = event.get("DTSTART").dt
-            event_end = event.get("DTEND").dt
-
-            if isinstance(event_start, dt.datetime):
-                new_event["allday"] = False
-                new_event["startDatetime"] = event_start.astimezone(local_timezone)
-                new_event["endDatetime"] = event_end.astimezone(local_timezone)
-            elif isinstance(event_start, dt.date):
-                new_event["allday"] = True
-                # Convert date into datetime at midnight
-                new_event["startDatetime"] = local_timezone.localize(
-                    dt.datetime.combine(event_start, dt.time(0, 0))
-                )
-                new_event["endDatetime"] = local_timezone.localize(
-                    dt.datetime.combine(event_end, dt.time(0, 0))
-                ) - dt.timedelta(minutes=1)
+        ics_urls = ics_url.split("|")
+        for ics_url in ics_urls:
+            response = requests.get(ics_url)
+            if response.ok:
+                cal = icalendar.Calendar.from_ical(response.text)
             else:
-                raise TypeError(f"Unknown type {type(event_start)} for DTSTART")
+                self.logger.error(f"Received an error when downloading ICS: {response.text}")
+                sys.exit(1)
 
-            new_event["isMultiday"] = (
-                new_event["endDatetime"] - new_event["startDatetime"]
-            ) > dt.timedelta(days=1)
+            cal_name = cal['X-WR-CALNAME']
 
-            if (
-                new_event["endDatetime"] >= calStartDatetime
-                and new_event["startDatetime"] < calEndDatetime
-            ):
-                # Don't show past days for ongoing multiday event
-                new_event["startDatetime"] = max(new_event["startDatetime"], calStartDatetime)
+            events = recurring_ical_events.of(cal).between(calStartDatetime, calEndDatetime)
+            local_timezone = pytz.timezone(localTZ)
 
-                event_list.append(new_event)
+            for event in events:
+                new_event = {"summary": str(event.get("SUMMARY"))}
+
+                if "LOCATION" in event:
+                    new_event["location"] = str(event.get("LOCATION"))
+
+                event_start = event.get("DTSTART").dt
+                event_end = event.get("DTEND").dt
+
+                if isinstance(event_start, dt.datetime):
+                    new_event["allday"] = False
+                    new_event["startDatetime"] = event_start.astimezone(local_timezone)
+                    new_event["endDatetime"] = event_end.astimezone(local_timezone)
+                elif isinstance(event_start, dt.date):
+                    new_event["allday"] = True
+                    # Convert date into datetime at midnight
+                    new_event["startDatetime"] = local_timezone.localize(
+                        dt.datetime.combine(event_start, dt.time(0, 0))
+                    )
+                    new_event["endDatetime"] = local_timezone.localize(
+                        dt.datetime.combine(event_end, dt.time(0, 0))
+                    ) - dt.timedelta(minutes=1)
+                else:
+                    raise TypeError(f"Unknown type {type(event_start)} for DTSTART")
+
+                new_event["isMultiday"] = (
+                    new_event["endDatetime"] - new_event["startDatetime"]
+                ) > dt.timedelta(days=1)
+
+                if (
+                    new_event["endDatetime"] >= calStartDatetime
+                    and new_event["startDatetime"] < calEndDatetime
+                ):
+                    # Don't show past days for ongoing multiday event
+                    new_event["startDatetime"] = max(new_event["startDatetime"], calStartDatetime)
+                    new_event["calendarName"] = cal_name
+
+                    event_list.append(new_event)
 
         return sorted(event_list, key=lambda k: k["startDatetime"])
